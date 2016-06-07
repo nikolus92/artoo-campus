34785dab27ecf145703d26c2eb34337d7384cc78
niki <nikolus92@gmail.com>
2016-06-07 16:24:18 +0000

    1nodo

--- client/subjects/init/app.js
+++ client/subjects/init/app.js
@@ -1,2 +1,18 @@
 // define app module
 // define a run function
+
+angular  // setter
+    .module('artooInit', [])
+    .run(function(){
+        console.info('setter');
+    });
+    
+var app = angular  // get
+    .module('artooInit')       // concatenazione funzioni
+    .run(function (){
+            console.info('getter');
+    });
+    
+    app.run(function(){
+         console.info('run var');   // esecuzione variabile
+    });
